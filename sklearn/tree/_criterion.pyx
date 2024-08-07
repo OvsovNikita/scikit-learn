f0122c436c12a702d8a1130329d17de1566d7f39
nikita <nikita@quantumlightcapital.com>
2024-08-07 19:10:47 +0100

    exps

--- sklearn/tree/_criterion.pyx
+++ sklearn/tree/_criterion.pyx
@@ -746,7 +746,7 @@ cdef class Gini(ClassificationCriterion):
         cdef intp_t k
         cdef intp_t c
 
-        cdef float64_t w_1 = 0.7
+        cdef float64_t w_1 = 0.999
         cdef float64_t w_c
         cdef float64_t p_c
 
